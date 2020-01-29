baf1b1b2d880d9b170bf361d6269ec7b35a7201a
Vadim <korobeinikovv@mail.ru>
2020-01-29 17:04:47 +0300

    Added router

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { SigninViewContainer } from "./components/views";
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <SigninViewContainer />
+        </Route>
+        <Route path="/signup">
+          <span>Sign Up</span>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
