c5b70f88ac1523c60a9e4e6ae31fa7a04a9a0ae9
Vadim <korobeinikovv@mail.ru>
2020-01-29 18:33:20 +0300

    Added signin view base layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 
 import { Router } from "./Router";
 import "./App.css";
@@ -7,19 +6,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Router />
     </div>
   );
---
+++ src/components/views/Signin/SigninViewContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { SigninView } from "./SigninView";
+
+SigninViewContainer.propTypes = {};
+
+export function SigninViewContainer(params) {
+  return <SigninView />;
+}
