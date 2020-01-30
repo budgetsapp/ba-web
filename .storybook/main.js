51795115ce4fea1dc26cd88ce2bf31842a853f24
Vadim <korobeinikovv@mail.ru>
2020-01-30 23:15:13 +0300

    Added sb addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js']
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-notes/register'
+  ]
 };
