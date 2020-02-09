5ec03a5ec99ba7974b7042ba1a4d6bb015eeac29
Vadim <korobeinikovv@mail.ru>
2020-02-10 00:32:12 +0300

    Removed unnecessary code

--- src/components/organisms/SigninForm/SigninFormContainer.jsx
+++ src/components/organisms/SigninForm/SigninFormContainer.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { SigninForm } from './SigninForm';
-import { storage } from '../../../services/storage';
 import { getIsAuthenticated, login as authLogin } from '../../../services/auth';
 import { appPath } from '../../../services/app-path';
-import { Storage } from '../../../consts/storage';
 
 SigninFormContainer.propTypes = {};
 
@@ -18,10 +16,6 @@ export function SigninFormContainer() {
   async function login({ login, password }, setSubmitting) {
     try {
       const res = await authLogin(login, password);
-      console.log(
-        'access_token recieved',
-        storage.getItem(Storage.ACCESS_TOKEN_KEY)
-      );
       console.log(JSON.stringify(res, null, 2));
       history.push(appPath.dashboard());
     } catch (e) {
